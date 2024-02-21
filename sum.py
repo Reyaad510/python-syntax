c7903ca5bf101c8d5cf2af40f1d0a0f1a1e3deb3
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-02-20 21:49:25 -0500

    Completed sum.py

--- sum.py
+++ sum.py
@@ -10,8 +10,11 @@ def sum_nums(nums):
 
     # Python has a built-in function `sum()` for this, but we don't
     # want you to use it. Please write this by hand.
+    total = 0
 
-    # YOUR CODE HERE
+    for num in nums:
+        total = total + num
+    return total
 
 
 print("sum_nums returned", sum_nums([1, 2, 3, 4]))
